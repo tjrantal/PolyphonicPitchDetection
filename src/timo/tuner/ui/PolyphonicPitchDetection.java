19f4888bb58e6cac452588eb56cc71af3ad5023c
Timo Rantalainen <tjrantal@gmail.com>
2014-06-08 14:24:08 +1000

    Started mofidying

--- src/timo/tuner/ui/PolyphonicPitchDetection.java
+++ src/timo/tuner/ui/PolyphonicPitchDetection.java
@@ -21,14 +21,14 @@
 /*
 Written by Timo Rantalainen tjrantal@gmail.com 2010 (C++ version) - 2012 (Java version)
 Based on Anssi Klapuri's (list of publications http://www.cs.tut.fi/~klap/iiro/ and http://www.elec.qmul.ac.uk/people/anssik/publications.htm) congress publication
-Klapuri, A., " Multiple fundamental frequency estimation by summing harmonic amplitudes," 7th International Conference on Music Information Retrieval (ISMIR-06), Victoria, Canada, Oct. 2006. 
+Klapuri, A., " Multiple fundamental frequency estimation by summing harmonic amplitudes," 7th International Conference on Music Information Retrieval (ISMIR-06), Victoria, Canada, Oct. 2006.
 http://www.cs.tut.fi/sgn/arg/klap/klap2006ismir.pdf
  and doctoral thesis:
 Klapuri, A. " Signal processing methods for the automatic transcription of music," Ph.D. thesis, Tampere University of Technology, Finland, April 2004.
- http://www.cs.tut.fi/sgn/arg/klap/phd/klap_phd.pdf  
+ http://www.cs.tut.fi/sgn/arg/klap/phd/klap_phd.pdf
 
 Contributions from other people taken from the internet (in addition to Java-tutorials for GUI, sound capture etc.)
-FFT-transform					
+FFT-transform
 
 Required class files (in addition to this one..).
 ReadStratecFile.java		//Stratec pQCT file reader
@@ -44,7 +44,7 @@ Analysis/Analysis.java \
 Analysis/Complex.java \
 Analysis/FFT.java \
 Analysis/Functions.java \
-Analysis/Klapuri.java  
+Analysis/Klapuri.java
 JAR building:
 jar cfe PolyphonicPitchDetection.jar ui.PolyphonicPitchDetection ui DrawImage Analysis Capture
 
@@ -59,6 +59,7 @@ import java.awt.geom.Line2D;
 import javax.swing.event.*;
 import javax.swing.border.*;
 import java.util.Vector;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.io.*;
 import javax.sound.sampled.*;
@@ -71,7 +72,7 @@ import java.awt.image.DataBuffer;
 import timo.tuner.Capture.*;	//Sound capture
 import timo.tuner.DrawImage.*;		//Drawing images
 
-public class PolyphonicPitchDetection extends JPanel implements ActionListener {	
+public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 	JButton beginPitchDetection;
 	JButton endPitchDetection;
 	public DrawImage fftFigure;
@@ -81,14 +82,15 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 	public float samplingRate = 44100;
 	public static int imWidth =800;
 	public static int imHeight =250;
+	public static int harmonics = 20;
 	public boolean continueCapturing;
 	public static int w;
-	public static int h; 
+	public static int h;
 	static int traces = 2;		/*how many traces are we plotting...*/
 	public double[] cb;			/*Klapuri whitening ranges*/
 	public double[] freq;		/*FFT fequency bins*/
 	public double[] f0cands;	/*Klapuri F0 candidates*/
-	public int[] f0index;		/*Klapuri F0 candidate indices*/
+	public ArrayList<Integer>[] f0index;		/*Klapuri F0 candidate indices*/
 	public PolyphonicPitchDetection(){ /*Constructor*/
 
 		JPanel buttons = new JPanel(); /*Panel for start and stop*/
@@ -98,7 +100,7 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 		beginPitchDetection.setActionCommand("beginPitchDetection");
 		beginPitchDetection.addActionListener(this);
 		beginPitchDetection.setToolTipText("Press to Begin pitch detection");
-		
+
 		/*End button*/
 		buttons.add(beginPitchDetection);
 		endPitchDetection= new JButton("End pitch detection");
@@ -109,7 +111,7 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 		endPitchDetection.setEnabled(false);
 		buttons.add(endPitchDetection);
 		add(buttons);
-		
+
 
 		/*Figure for captured sound*/
 		rawFigure = new DrawImage();
@@ -136,7 +138,7 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if ("beginPitchDetection".equals(e.getActionCommand())) {
 			endPitchDetection.setEnabled(true);
-			beginPitchDetection.setEnabled(false);	
+			beginPitchDetection.setEnabled(false);
 			/*Create constant arrays for Klapuri*/
 			cb = new double[32];
 			/*CB filterbank always the same values, could be included from somewhere...*/
@@ -147,7 +149,7 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 			captured signal will be zero padded to twice its length
 			*/
 			freq = new double[(int) Math.floor((double) fftWindow)];
-			for (int b = 0;b<Math.floor((double) fftWindow);b++){
+			for (int b = 0;b<Math.floor((double) fftWindow);++b){
 				freq[b] = (double) b*(double)samplingRate/(double) fftWindow;
 			}
 			int temp = 0;
@@ -158,33 +160,65 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 				}
 				kk++;
 			}
-			f0cands = new double[temp];
-			f0index = new int[temp];
-			kk=0;
-			temp = 0;
-			while (freq[kk] <= 1500.0){
-				if (freq[kk] >=60.0){
-					f0cands[temp] = freq[kk];
-					f0index[temp] = kk;
-					++temp;				
-				}
-				kk++;
+
+			/*
+			*Create candidate frequencies here (http://www.phy.mtu.edu/~suits/NoteFreqCalcs.html)
+			*Five octaves of candidate notes. Use quarter a half-step to get out of tune freqs
+			*Lowest freq (f0) = 55.0 Hz, A three octaves below A above the middle C
+			*/
+			double f0Init = 55;	//Hz
+			double a = Math.pow(2.0,(1.0/12.0));
+			f0cands = new double[5*12*4];	//5 octaves, 12 half-steps per octave, quarter half-steps
+			for (kk = 0;kk<f0cands.length;++kk){
+			   f0cands[kk] = f0Init*Math.pow(a,((double)kk)/4.0);
 			}
-			
+
+			/*
+			*Pre-calculate frequency bins for  a given f0 candidate
+			*	halfBinWidth = ((constants.samplingRate/2)/constants.fftWindow)/2;
+	for i = 1:length(constants.f0cands)
+		binIndices = [];
+		for h = 1:constants.harmonics
+			testi = find(constants.freq > (constants.f0cands(i)*h-halfBinWidth) & constants.freq < (constants.f0cands(i)*h+halfBinWidth));
+			binIndices = [binIndices testi];
+		end
+		constants.f0candsFreqBins(i).binIndices = binIndices;
+	end
+			*/
+			 f0index = new ArrayList[f0cands.length];
+			double halfBinWidth= ((double)samplingRate/(double) fftWindow)/2.0;
+			for (int k =0;k<f0index.length;++k){
+			   f0index[k] = new ArrayList();
+			   for (int h =0; h< harmonics;++h){
+			      f0index[k].addAll(find(freq,f0cands[k]*((double)h)-halfBinWidth,f0cands[k]*((double)h)+halfBinWidth));
+			   }
+			}
+
+
+
 			continueCapturing = true;
 			/*Start capturing and analysis thread*/
 			//Capture capture = new Capture(samplingRate,8,this);
 			Capture capture = new Capture(16,this);
 			Thread captureThread = new Thread(capture,"captureThread");
-			captureThread.start();	
-		}	
+			captureThread.start();
+		}
 		if ("endPitchDetection".equals(e.getActionCommand())){
 			continueCapturing = false;
 			endPitchDetection.setEnabled(false);
 			beginPitchDetection.setEnabled(true);
-		}		
+		}
 	}
 
+	private ArrayList<Integer> find(double[] arr, double lower, double upper){
+	   ArrayList<Integer> b = new ArrayList<Integer>();
+	   for (int i = 0; i<arr.length;++i){
+	      if (arr[i]>=lower && arr[i] <=upper){
+	      	b.add(i);
+	      }
+	   }
+	   return b;
+	}
 	public static void initAndShowGUI(){
 		JFrame f = new JFrame("Polyphonic Pitch Detection");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -201,8 +235,8 @@ public class PolyphonicPitchDetection extends JPanel implements ActionListener {
 		f.setSize(w, h);
 		f.setVisible(true);
 	}
-	
-	
+
+
 	public static void main(String[] args){
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run(){
